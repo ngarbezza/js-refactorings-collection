9b517d497173f1cdfdfab8c9c3b4bd0ddb750565
Nahuel Garbezza <n.garbezza@gmail.com>
2021-02-19 07:46:06 -0300

    initial commit: introduce "remove unnecessary conditional" refactoring

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# js-refactorings-collection
-A collection of simple refactorings for Javascript
+# Javascript Refactorings Collection
+
+A collection of simple refactorings for Javascript. It uses [Acorn](https://github.com/acornjs/acorn) for parsing the JS code.
+
+## Why
+
+Implementing refactorings is not that hard as it seems. We as developers should write our own tools if we don't have them available in our programming environments.
+
+## Refactorings list
+
+### Remove unnecessary conditional
+
+Input:
+
+```javascript
+if (a > b) {
+  return true;
+} else {
+  return false;
+}
+```
+
+Output:
+
+```javascript
+return a > b; 
+```
+
+Usage:
+
+```javascript
+const RemoveUnnecessaryConditional = require('lib/remove_unnecessary_conditional');
+
+const originalCode = 'if (a > b) { return true } else { return false }';
+const refactoring = new RemoveUnnecessaryConditional(originalCode);
+const refactoredCode =  refactoring.apply();
+// refactoredCode: 'return a > b'
+```
+
+## Next Steps
+
+- [ ] release NPM package
+- [ ] VSCode integration
+- [ ] Continue adding refactorings
---
+++ test/remove_unnecessary_conditional_test.js
@@ -0,0 +1,87 @@
+const { suite, test, assert } = require('@pmoo/testy');
+
+const RemoveUnnecessaryConditional = require('../lib/remove_unnecessary_conditional');
+
+function removeUnnecessaryIf(sourceCode) {
+  return new RemoveUnnecessaryConditional(sourceCode).apply();
+}
+
+suite('"remove unnecessary conditional" refactoring', () => {
+  test('works on a trivial condition', () => {
+    const sourceCode = 'if (true) return true; else return false';
+    const newSourceCode = removeUnnecessaryIf(sourceCode);
+
+    assert.areEqual(newSourceCode, 'return true');
+  });
+
+  test('works on a non-trivial condition', () => {
+    const sourceCode = 'if ([1,2,3].length > 2) return true; else return false';
+    const newSourceCode = removeUnnecessaryIf(sourceCode);
+
+    assert.areEqual(newSourceCode, 'return [1,2,3].length > 2');
+  });
+
+  test('fails when the code is not an if statement', () => {
+    const sourceCode = 'while ([1,2,3].length > 2) return true';
+
+    assert
+      .that(() => removeUnnecessaryIf(sourceCode))
+      .raises(new Error(RemoveUnnecessaryConditional.errorMessageForInvalidStatementType()));
+  });
+
+  test('fails when there are more than one statement', () => {
+    const sourceCode = 'if ([1,2,3].length > 2) return true; const x = 42;';
+
+    assert
+      .that(() => removeUnnecessaryIf(sourceCode))
+      .raises(new Error(RemoveUnnecessaryConditional.errorMessageForMultipleStatements()));
+  });
+
+  test('fails when the consequent block is not a return statement', () => {
+    const sourceCode = 'if ([1,2,3].length > 2) { const x = 42; } else return false';
+
+    assert
+      .that(() => removeUnnecessaryIf(sourceCode))
+      .raises(new Error(RemoveUnnecessaryConditional.errorMessageForConditionalCannotBeSimplified()));
+  });
+
+  test('fails when the consequent block is not a { return true } statement', () => {
+    const sourceCode = 'if ([1,2,3].length > 2) return 42; else return false';
+
+    assert
+      .that(() => removeUnnecessaryIf(sourceCode))
+      .raises(new Error(RemoveUnnecessaryConditional.errorMessageForConditionalCannotBeSimplified()));
+  });
+
+  test('fails when the alternative block is not a return statement', () => {
+    const sourceCode = 'if ([1,2,3].length > 2) return true; else { const x = 42; }';
+
+    assert
+      .that(() => removeUnnecessaryIf(sourceCode))
+      .raises(new Error(RemoveUnnecessaryConditional.errorMessageForConditionalCannotBeSimplified()));
+  });
+
+  test('fails when the alternative block is not a { return false } statement', () => {
+    const sourceCode = 'if ([1,2,3].length > 2) return true; else return 42';
+
+    assert
+      .that(() => removeUnnecessaryIf(sourceCode))
+      .raises(new Error(RemoveUnnecessaryConditional.errorMessageForConditionalCannotBeSimplified()));
+  });
+
+  test('fails when the alternative block is not present', () => {
+    const sourceCode = 'if ([1,2,3].length > 2) return true';
+
+    assert
+      .that(() => removeUnnecessaryIf(sourceCode))
+      .raises(new Error(RemoveUnnecessaryConditional.errorMessageForAlternativeBlockNotPresent()));
+  });
+
+  test('fails when the code has syntax errors', () => {
+    const sourceCode = 'if (true))) return true';
+
+    assert
+      .that(() => removeUnnecessaryIf(sourceCode))
+      .raises(/the code has syntax errors/);
+  });
+});
